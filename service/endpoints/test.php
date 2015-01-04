539c1926706df63450c3ccd73388743944bf4dce
Ron <me@ronaldarichardson.com>
2015-01-03 16:23:08 -0800

    last stuff

--- service/endpoints/test.php
+++ service/endpoints/test.php
@@ -2,8 +2,4 @@
 /**
  * The test endpoint for testing
  */
-$endpoint = $skeleton->new->Endpoint('Test', function($skeleton, $endpoint, $request) {
-	if($request->method() == 'GET') {
-		Skeleton_Helper::say('Hello World!');
-	}
-});
+$endpoint = $skeleton->new->Endpoint('Test')->setTable('users')factoryOutput();
